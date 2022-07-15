651579066cc72c38e4fcd7c9ff44ee7cbe735f1c
ShahadilNaz <109242147+ShahadilNaz@users.noreply.github.com>
2022-07-15 12:39:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ TOKEN = os.environ.get("TOKEN")
 print(TOKEN)
 bot = telebot.TeleBot(TOKEN)
 
+count = 0
 @bot.message_handler(commands=["start"])
 def start(msg: telebot.types.Message):
     while True:
-        bot.reply_to(msg, f"Hi {msg.chat.first_name}, How are you doing today :)")
-        
+        bot.reply_to(msg, f"Hi {msg.chat.first_name}, How are you doing today :)\nCount: {count}")
+        count += 1
+        time.sleep(300)
 bot.polling()
